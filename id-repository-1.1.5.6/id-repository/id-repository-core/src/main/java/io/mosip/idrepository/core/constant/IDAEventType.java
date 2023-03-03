697669da1a522640686b3c30a486687d714e5d5e
surafeal <suradirriba16@gmail.com>
2023-03-03 04:59:51 -0800

    id-repository-test

---
+++ id-repository-1.1.5.6/id-repository/id-repository-core/src/main/java/io/mosip/idrepository/core/constant/IDAEventType.java
@@ -0,0 +1,11 @@
+package io.mosip.idrepository.core.constant;
+
+/**
+ * @author Manoj SP
+ *
+ */
+public enum IDAEventType implements EventType {
+
+	CREDENTIAL_ISSUED, REMOVE_ID, DEACTIVATE_ID, ACTIVATE_ID, AUTH_TYPE_STATUS_UPDATE, 
+	IDENTITY_CREATED, IDENTITY_UPDATED;
+}
